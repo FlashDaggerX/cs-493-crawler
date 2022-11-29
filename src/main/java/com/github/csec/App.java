926e31d98ec5a7269d517a4687768e854af51b08
Kyle Guarco <kyleguarco55@gmail.com>
2022-11-28 19:44:30 -0500

    JSoup Hello World!

--- src/main/java/com/github/csec/App.java
+++ src/main/java/com/github/csec/App.java
@@ -1,7 +1,25 @@
 package com.github.csec;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class App {
+    // Define the user agent for this scraper, which is a mobile device using Chrome
+    private static final String USER_AGENT = "Mozilla/5.0 (Linux; Android 11; SAMSUNG SM-G973U) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/14.2 Chrome/87.0.4280.141 Mobile Safari/537.36";
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Document soup = null;
+
+        // Attempt to get the document and error otherwise.
+        try {
+            soup = Jsoup.connect("https://footlocker.com").userAgent(USER_AGENT).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Get the header tags and print out the inner text on each one
+        soup.select("h1").forEach((e) -> System.out.println(e.text()));
     }
 }
