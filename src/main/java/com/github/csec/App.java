51ed98c802511440721d4a5309ebb2655fd6ee33
Chan Nguyen <nevvrevolution101@gmail.com>
2022-11-30 15:05:08 -0500

    Added price and working on original and final prices

--- src/main/java/com/github/csec/App.java
+++ src/main/java/com/github/csec/App.java
@@ -4,22 +4,38 @@ import java.io.IOException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class App {
-    // Define the user agent for this scraper, which is a mobile device using Chrome
-    private static final String USER_AGENT = "Mozilla/5.0 (Linux; Android 11; SAMSUNG SM-G973U) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/14.2 Chrome/87.0.4280.141 Mobile Safari/537.36";
+  // Define the user agent for this scraper, which is a mobile device using Chrome
+  private static final String USER_AGENT =
+      "Mozilla/5.0 (Linux; Android 11; SAMSUNG SM-G973U) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/14.2 Chrome/87.0.4280.141 Mobile Safari/537.36";
 
-    public static void main(String[] args) {
-        Document soup = null;
-        final String url = "https://footlocker.com/search";
-        // Attempt to get the document and error otherwise.
-        try {
-            soup = Jsoup.connect(url).userAgent(USER_AGENT).data("query", "blue").get();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // Get the header tags and print out the inner text on each one
-        soup.select("span[class=ProductName-primary]").forEach((e) -> System.out.println(e.text()));
+  public static void main(String[] args) {
+    Document soup = null;
+    final String url = "https://footlocker.com/search";
+    // Attempt to get the document and error otherwise.
+    try {
+      soup = Jsoup.connect(url).userAgent(USER_AGENT).data("query", "blue").get();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+
+    // Get the header tags and print out the inner text on each one
+    soup.select("div[class=ProductCard hasVariants]").forEach((e) -> {
+      String shoeName = e.selectFirst("span[class=ProductSale-primary]").text();
+      String price = "";
+      try {
+        Element sale = e.expectFirst(url)
+      } catch (IllegalArgumentException e) {
+        
+      }
+      String priceOriginal = e.selectFirst("span[class=ProductPrice]").firstElementChild().text();
+      String priceFinal = e.selectFirst("span[class=ProductPrice]").firstElementChild().text();
+      
+      System.out.println(shoeName + " " + price);
+    });
+    
+    
+  }
 }
